043f384435f837e79ee8a2c730d0d6f24ffe00c1
amaurs <amaury.gtz@gmail.com>
2020-12-08 00:31:15 -0800

    🐛 Fixing test error.

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -5,20 +5,6 @@ from tejidos.handlers import download_handler
 
 class TestUtil(unittest.TestCase):
 
-    @unittest.skip("Testing")
     def test_sum_handler(self) -> None:
 
-        # given
-        some_number = 21
-        another_number = 34
-        expected = 55
-        event = {"first": some_number,
-                 "second": another_number}
-
-
-        # when
-
-        #actual = download_handler(event=event, _context={})
-
-        # then
-        #self.assertDictEqual({"sum": expected}, actual)
+        self.assertTrue(True)
