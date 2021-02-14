c6b2cb067bd04104a87aabd0b2f8cbcd9ae65ae7
amaurs <amaury.gtz@gmail.com>
2021-02-13 16:24:47 -0800

    💽 Adding SQLAlchemy and postgres database to the project.

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,10 +1,20 @@
 from flask.cli import FlaskGroup
 
-from tejidos.app import app
-
+from tejidos.app import app, db, User
 
 cli = FlaskGroup(app)
 
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@cli.command("seed_db")
+def seed_db():
+    db.session.add(User(email="michael@mherman.org"))
+    db.session.commit()
 
 if __name__ == "__main__":
     cli()
